18e7ed072d2e376d71ff5e02a839fc3141f6cc0d
NohYeaJin <yeajinny@naver.com>
2022-03-29 20:54:24 +0900

    Create baekjoon_13458.py

---
+++ baekjoon_13458.py
@@ -0,0 +1,13 @@
+import sys
+num = int(sys.stdin.readline())
+num_list = list(map(int,sys.stdin.readline().split()))
+a,b = map(int,sys.stdin.readline().split())
+total = 0
+for i in num_list:
+    if i>a:
+        if ((i-a)/b)- ((i-a)//b) > 0:
+            total += ((i-a)//b + 1)
+        else:
+            total += (i-a)//b
+print(total+len(num_list))
+
